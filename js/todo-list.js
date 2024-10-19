bd47e6c22183f955e2d0a60a1d1ae252c265c4a2
iambibek-1 <iambibek100@gmail.com>
2024-10-19 10:04:32 +0545

    keep learning

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,5 +1,13 @@
-const todoList = [];
+const todoList = ["Wake up","eat breakfast"];
 
+let todoListHtml = '';
+
+for (let i=0;i<todoList.length;i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHtml += html;
+}
+console.log(todoListHtml);
 function addtodo(){
     const inputelement = document.querySelector('.input-todo');
     const todoValue = inputelement.value;
